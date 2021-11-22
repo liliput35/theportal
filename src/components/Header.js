6df774b928297c3484560905a3512c7eb7b165ab
liliput35 <83822871+liliput35@users.noreply.github.com>
2021-11-22 18:14:59 +0800

    responsive imporvements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Header = () => {
+    const [navClick, setNavClick] = useState('burger')
+    const [nav, setNav] = useState('nonenav')
+
+    const toggleNav = () => {
+        if(navClick === 'burger toggle'){
+            setNavClick('burger')
+            setNav('nonenav')
+        }else{
+            setNavClick('burger toggle')
+            setNav('display')
+        }
+    }
+
     return (
         <header>
             <div className="navbar" id="header">
                 <div className="container">
                     <h3>The Portal</h3>
 
-                    <ul>
+                    <div className={navClick} onClick={toggleNav}>
+                        <div className="line1"></div>
+                        <div className="line2"></div>
+                        <div className="line3"></div>
+                    </div>
+
+                    <ul className={nav}>
                         <li><a href="#">Home</a></li>
                         <li><a href="#movies">Movies</a></li>
                         <li><a href="#about">About</a></li>
                     </ul>
+                    
                 </div>
             </div>
         </header>
