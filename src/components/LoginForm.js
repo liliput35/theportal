2133495c50809e601b2a7480eb2c36fe55f430c9
liliput35 <83822871+liliput35@users.noreply.github.com>
2021-11-22 14:07:42 +0800

    turned off autofill

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -15,11 +15,11 @@ const LoginForm = ({ Login, error, lclasss }) => {
                 <div className="wrapper">
                     <h1>Welcome to The Portal</h1>
 
-                    <input type="text" name="name" id="name" placeholder="Enter Name" autoComplete="0" 
+                    <input type="text" name="name" id="name" placeholder="Enter Name" autoComplete="false" 
                     onChange={e => setDetails({...details, name: e.target.value})} value={details.name}/> <br />
 
                     <input type="password" name="password" id="password" placeholder="Enter Password"
-                    onChange={e => setDetails({...details, password: e.target.value})} value={details.password}/> <br />
+                    onChange={e => setDetails({...details, password: e.target.value})} value={details.password} autoComplete="false"/> <br />
 
                     <input type="submit" value="LOGIN" className="submit"/>
                 </div>
